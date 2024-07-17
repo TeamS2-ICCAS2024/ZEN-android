1303ed5091e6bb0a151c0c049a8311dacb115541
Hyunuk Shin <shu914@naver.com>
2024-07-17 14:11:39 +0900

    feat: 설정 창 네비게이션 연결 (#27)

---
+++ app/src/main/java/com/iccas/zen/presentation/home/SettingScreen.kt
@@ -0,0 +1,13 @@
+package com.iccas.zen.presentation.home
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.iccas.zen.presentation.components.BasicBackgroundWithLogo
+
+
+@Composable
+fun SettingScreen(navController: NavController) {
+    BasicBackgroundWithLogo {
+
+    }
+}
