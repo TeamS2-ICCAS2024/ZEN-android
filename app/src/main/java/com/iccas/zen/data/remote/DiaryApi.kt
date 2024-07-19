555f5bfc049ffa19e183e66bf2b0a9cc6e7e4e21
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-07-20 02:42:35 +0900

    chore : 불필요 import문 삭제

--- app/src/main/java/com/iccas/zen/data/remote/DiaryApi.kt
+++ app/src/main/java/com/iccas/zen/data/remote/DiaryApi.kt
@@ -2,7 +2,6 @@ package com.iccas.zen.data.remote
 
 import com.iccas.zen.data.dto.auth.request.EmotionDiaryPostRequest
 import com.iccas.zen.data.dto.auth.response.ApiResponse
-import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
--- app/src/main/java/com/iccas/zen/presentation/chatBot/SelectEmotionScreen.kt
+++ app/src/main/java/com/iccas/zen/presentation/chatBot/SelectEmotionScreen.kt
@@ -12,14 +12,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
-import com.iccas.zen.R
 import com.iccas.zen.presentation.chatBot.ChatViewModel
 import com.iccas.zen.presentation.components.BasicBackgroundWithLogo
 import com.iccas.zen.presentation.chatBot.components.TopBar
