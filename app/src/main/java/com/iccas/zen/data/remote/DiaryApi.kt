14b56ba5b320b322ea91d988f4a43f39a9db20f1
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-07-22 16:05:25 +0900

    feat : api 연결

--- app/src/main/java/com/iccas/zen/data/remote/DiaryApi.kt
+++ app/src/main/java/com/iccas/zen/data/remote/DiaryApi.kt
@@ -1,11 +1,20 @@
 package com.iccas.zen.data.remote
 
-import com.iccas.zen.data.dto.auth.request.EmotionDiaryPostRequest
+import com.iccas.zen.data.dto.emotionDiary.request.EmotionDiaryPostRequest
 import com.iccas.zen.data.dto.ApiResponse
+import com.iccas.zen.data.dto.emotionDiary.response.DiaryListGetResponse
+import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface DiaryApi {
+
     @POST("/api/v1/emotion-diary")
     suspend fun postEmotionDiary(@Body emotionDiaryPostRequest: EmotionDiaryPostRequest): ApiResponse
+
+    @GET("/api/v1/diary-list")
+    //suspend fun  getDiaryList() : DiaryListGetResponse
+    suspend fun getDiaryList(): Response<DiaryListGetResponse>
+
 }
